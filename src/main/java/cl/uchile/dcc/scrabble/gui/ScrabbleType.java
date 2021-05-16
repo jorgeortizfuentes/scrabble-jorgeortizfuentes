888f8980e5dc896b8db82040a5764e16780a403c
Jorge Ortiz <jorge@ortizfuentes.com>
2021-05-16 18:35:25 -0400

    operations in interface

--- src/main/java/cl/uchile/dcc/scrabble/gui/ScrabbleType.java
+++ src/main/java/cl/uchile/dcc/scrabble/gui/ScrabbleType.java
@@ -4,21 +4,19 @@ public interface ScrabbleType {
     String toString();
     ScrabbleString asString();
     boolean equals(Object obj);
-    ScrabbleType add(ScrabbleType c);
-    ScrabbleType addedBy(ScrabbleType c);
-    ScrabbleType conjunction(ScrabbleType c);
-    ScrabbleType conjunctionBy(ScrabbleType c);
-    ScrabbleType disjunction(ScrabbleType c);
-    ScrabbleType disjunctionBy(ScrabbleType c);
-    ScrabbleType subtract(ScrabbleType c);
-    ScrabbleType subtractBy(ScrabbleType c);
-    ScrabbleType multiply(ScrabbleType c);
-    ScrabbleType multiplyBy(ScrabbleType c);
-    ScrabbleType divide(ScrabbleType c);
-    ScrabbleType divideBy(ScrabbleType c);
-    ScrabbleType negation(ScrabbleType c);
-    //int addTo();
-    //int addBy();
+    //ScrabbleType add(ScrabbleType c);
+    //ScrabbleType addedBy(ScrabbleType c);
+    //ScrabbleType conjunction(ScrabbleType c);
+    //ScrabbleType conjunctionBy(ScrabbleType c);
+    //crabbleType disjunction(ScrabbleType c);
+    //ScrabbleType disjunctionBy(ScrabbleType c);
+    //ScrabbleType subtract(ScrabbleType c);
+    //ScrabbleType subtractBy(ScrabbleType c);
+    //ScrabbleType multiply(ScrabbleType c);
+    //ScrabbleType multiplyBy(ScrabbleType c);
+    //ScrabbleType divide(ScrabbleType c);
+    //ScrabbleType divideBy(ScrabbleType c);
+    //ScrabbleType negation(ScrabbleType c);
 
 
 }
